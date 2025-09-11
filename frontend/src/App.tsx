dd1ade60c55d25a0aa7032966af3375680827c63
Ninad09 <ninadgaonkar234@gmail.com>
2025-09-12 01:37:28 +0530

    dashboard route changed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@
 import { Signin } from "./pages/Signin"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Signup } from "./pages/Signup"
-import { Dashboard } from "./pages/Dashboard"
+// import { Dashboard } from "./pages/Dashboard"
 import Home from "./pages/Home"
 
 function App() {
@@ -10,7 +10,6 @@ function App() {
   return <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home/>} />
-      <Route path="/dashboard" element={<Dashboard/>}/>
       <Route path="/signup" element={<Signup/>}/>
       <Route path="/signin" element={<Signin/>}/>
     </Routes>
